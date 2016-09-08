ab1b69473bac1e9d958e8ecf1bcd5c374fe36a2a
Billy Everyteen <rodolfolessa.93@gmail.com>
2016-09-08 20:21:57 -0300

    United States

---
+++ steinbeck.rb
@@ -0,0 +1,6 @@
+puts "And this I believe: that the free, exploring mind of the individual human is the most valuable 
+thing in the world. And this I would fight for: the freedom of the mind to take any direction it wishes, 
+undirected. And this I must fight against: any idea, religion, or government which limits or destroys the individual.
+This is what I am and what I am about"
+
+puts "Steinbeck"
