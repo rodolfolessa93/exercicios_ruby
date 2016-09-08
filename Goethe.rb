89103401d39e5b21e5590f71efb744057ffbf317
Billy Everyteen <rodolfolessa.93@gmail.com>
2016-09-08 20:34:29 -0300

    Literatura germânica

---
+++ Goethe.rb
@@ -0,0 +1,12 @@
+puts "FAUSTO
+Que sou eu, se não posso alcançar, afinal, 
+A coroa com louros da nossa humanidade, 
+A que todos almejam com tanta ansiedade? 
+
+MEFISTÓFELES
+Não és mais, meu senhor, do que és: um mortal!
+Perucas podes ter, com louros aos milhões. 
+Alçar-te com teus pés nos mais altos tacões, 
+Serás sempre o que és: um pobre ser mortal!"
+
+puts "Goethe"
