4e8114edd7f2547a9841de054faec2fe0d2e5844
Billy Everyteen <rodolfolessa.93@gmail.com>
2016-09-08 21:09:28 -0300

    Descrição de Dostoievski

--- dostoievski.rb
+++ dostoievski.rb
@@ -1,4 +1,4 @@
 puts "Mas onde teria ele ido buscar esses sentimentos? (...) Tal era o vazio que, de súbito, se
  apoderara do seu coração. Uma infinita solidão e alheiamento se revelava subitamente à sua consciência"
 
-puts "Dostoievski"
+puts "Dostoievski, escritor russo do século XIX"
